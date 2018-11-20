77002fa86a3ba6195a8e620a315224fc9d9f7217
Mark Schäfer <mark@git.de>
2018-11-20 21:00:49 +0100

    Output erzeugt

--- src/main/java/test/git/Main.java
+++ src/main/java/test/git/Main.java
@@ -6,15 +6,23 @@ import java.util.List;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		
+		System.out.println(new Main().fibbonaci(Integer.parseInt(args[0])));
 	}
-	
+
 	public List<Integer> fibbonaci(int n) {
 		LinkedList<Integer> result = new LinkedList<Integer>();
-	
-		
-		
+
+		int a = 1, b = 1;
+
+		result.add(a);
+		while (b <= n) {
+			result.add(b);
+
+			int c = a + b;
+			a = b;
+			b = c;
+
+		}
 		return result;
 	}
 
