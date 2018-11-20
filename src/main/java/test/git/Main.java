8fccf5aabbd5bee612a779c1982bdca548f88840
Mark Schäfer <mark@git.de>
2018-11-20 20:53:27 +0100

    Initial angelegt

---
+++ src/main/java/test/git/Main.java
@@ -0,0 +1,21 @@
+package test.git;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+	}
+	
+	public List<Integer> fibbonaci(int n) {
+		LinkedList<Integer> result = new LinkedList<Integer>();
+	
+		
+		
+		return result;
+	}
+
+}
