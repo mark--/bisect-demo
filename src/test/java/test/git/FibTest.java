a93cc81b8960bd27bb2759a1edf69da07702df30
Mark Schäfer <mark@git.de>
2018-11-21 08:00:44 +0100

    More Tests

--- src/test/java/test/git/FibTest.java
+++ src/test/java/test/git/FibTest.java
@@ -12,4 +12,19 @@ public class FibTest {
 	public void fib() {
 		assertEquals(List.of(1, 1, 2, 3), new Main().fibbonaci(3));
 	}
+
+	@Test
+	public void fib2() {
+		assertEquals(List.of(1, 1, 2, 3, 5, 8), new Main().fibbonaci(10));
+	}
+
+	@Test
+	public void fib3() {
+		assertEquals(List.of(), new Main().fibbonaci(0));
+	}
+
+	@Test
+	public void fib4() {
+		assertEquals(List.of(1, 1), new Main().fibbonaci(1));
+	}
 }
