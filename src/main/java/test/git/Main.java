73ba64e884ad5d74a17f35e454d240d1ddccd187
Mark Schäfer <it-consulting@markschaefer.de>
2018-11-22 10:27:46 +0100

    Hilfetext

--- src/main/java/test/git/Main.java
+++ src/main/java/test/git/Main.java
@@ -6,6 +6,14 @@ public class Main {
 
 	public static void main(String[] args) {
 
+		if (args.length >= 1) {
+			if (args[0].trim()
+			        .equals("-h")) {
+				System.out.println("Aufruf mit java -jar .. <MAX> <MUL>");
+				return;
+			}
+		}
+
 		String maxArgument = args.length > 0 ? args[0] : "100";
 		BigInteger max;
 		try {
